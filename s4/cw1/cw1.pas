f5f130cd1970ff1664453641c46213dab55e9392
Pythonic-Rainbow <wongchingho0@gmail.com>
2018-09-15 23:36:30 +0800

    Fix ord() syntax error

--- s4/cw1/cw1.pas
+++ s4/cw1/cw1.pas
@@ -5,13 +5,13 @@ var name:string;
 begin
     write('Enter your name : ');
     readln(name);
-    if (ord(name[1]) >= 96) and (ord(name[1] <= 123)) then
+    if (ord(name[1]) >= 96) and (ord(name[1]) <= 123) then
         name[1] := chr(ord(name[1]) - 32);
     for i := 2 to length(name) do
     begin
-        if (ord(name[i]) >= 96) and (ord(name[i] <= 123)) and (name[i-1] = ' ') then
+        if (ord(name[i]) >= 96) and (ord(name[i]) <= 123) and (name[i-1] = ' ') then
             name[i] := chr(ord(name[i]) - 32)
-        else if (ord(name[i]) >= 65) and (ord(name[i] <= 90)) then
+        else if (ord(name[i]) >= 65) and (ord(name[i]) <= 90) then
             name[i] := chr(ord(name[i]) + 32);
     end;
     writeln('Hello, ', name);
