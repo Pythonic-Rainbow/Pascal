55e26ff484025364f161c2a825aeb97dfc0e7fef
s141328 <S141328@STUDENT.STTSS>
2018-09-24 09:05:52 +0800

    Update HW2

--- s4/hw2/hw2.pas
+++ s4/hw2/hw2.pas
@@ -1,6 +1,6 @@
 Program s4_hw2;
 var
-    name: string;
+    name,p: string;
     sex: char;
     ID: array[1..8] of char;
     i,d: integer;
@@ -17,15 +17,18 @@ begin
         readln(sex);
     end;
     writeln('Enter your HKID :');
+    readln(p);
     for i := 1 to 8 do
-        readln(ID[i]);
-    d := 324 + (ord(ID[1]) - 55) * 8;
-    for i := 2 to 7 do
-        d := d + (ord(ID[i]) - 55) * (9 - i);  
+        ID[i] := p[i];
+    d := (ord(ID[1]) - 64) * 8;
+    for i := 2 to 8 do
+        d := d + (ord(ID[i]) - 48) * (9 - i);
+    if ID[8] = 'A' then
+        d := d - 6;
     //Fixes captial error
     for i := 1 to length(name) do
         if (ord(name[i]) >= 96) and (ord(name[i]) <= 123) then
-            name[i] := chr(ord(name[i]) - 31);
+            name[i] := chr(ord(name[i]) - 32);
     //Prepends gender prefix
     if (sex = 'm') or (sex = 'M') then
         name := 'Mr. ' + name
@@ -34,11 +37,7 @@ begin
     //Calculate check digit
     i := d mod 11;
     if i = 0 then
-        sex := '0'
-    else if i = 1 then
-        sex := 'A'
-    else
-        sex := chr(i + 37);
+        sex := ID[8];
     //Display result
     if sex = ID[8] then
         writeln(name + ' has entered a valid HKID no.')
