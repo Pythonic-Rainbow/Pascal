388fb35e74c461c2369fa2a34deee4e34a87037e
s141328 <S141328@STUDENT.STTSS>
2018-10-04 09:01:52 +0800

    HW3 trio integers

--- s4/hw3/hw3.pas
+++ s4/hw3/hw3.pas
@@ -1,19 +1,21 @@
 Program s4_hw3;
 var
-    a,b,h,i:integer;
+    a,b,c,h,i:integer;
 begin
     writeln('Enter first integer');
     readln(a);
     writeln('Enter second integer');
     readln(b);
+    writeln('Enter third integer');
+    readln(c);
     i := a + 1;
     repeat
         i := i - 1;
-    until (b mod i = 0) and (a mod i = 0);
+    until (b mod i = 0) and (a mod i = 0) and (c mod i = 0);
     h := i;
     i := 0;
     repeat
         i := i + 1;
-    until (a * i mod b = 0);
+    until (a * i mod b = 0) and (a * i mod c = 0);
     writeln('HCF: ' , h , ' LCM: ' , i*a);
 end.
