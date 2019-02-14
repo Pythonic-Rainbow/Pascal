7d8f0dfefef8611b9e25ae0fdff0457c61e142cb
s141328 <S141328@imail.sttss.edu.hk>
2019-02-14 08:42:39 +0800

    CW6

---
+++ s4/cw6/cw6_write.pas
@@ -0,0 +1,12 @@
+Program cw6_write;
+var
+    f: text;
+    i: integer;
+
+begin
+    Assign(f, 'odd.txt');
+    rewrite(f);
+    for i := 0 to 49 do
+        writeln(i * 2 + 1, f);
+    close(f);
+end.
