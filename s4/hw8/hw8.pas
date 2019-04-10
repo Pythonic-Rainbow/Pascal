17f801caf77f9c4a1e34476a9cef8c9b5c2d5fde
s141328 <S141328@imail.sttss.edu.hk>
2019-04-10 09:24:55 +0800

    Complete HW8

--- s4/hw8/hw8.pas
+++ s4/hw8/hw8.pas
@@ -8,15 +8,15 @@ var
 
 procedure readFile();
 begin
-    assign(dat, 'phone.dat');
     reset(dat);
-    pointer := 1;
+    pointer := 0;
     while not eof(dat) do begin
+        pointer := pointer + 1;
         readln(dat, fname[pointer]);
         readln(dat, lname[pointer]);
-        readln(dat, phone[pointer]);
+        readln(dat, phone[pointer]);      
     end;
-    writeln('Read ',pointer-1, 'entries.');
+    writeln('Read ',pointer, ' entries.');
     close(dat);
 end;
 
@@ -28,13 +28,28 @@ begin
         if fname[i] = first then getIndex := i;
 end;
 
-procedure append(first:string; last:string; fone:string);
+procedure writeFile();
+var i : integer;
 begin
     rewrite(dat);
+    i := 0;
+    repeat
+        i := i + 1;
+        writeln(dat, fname[i]);
+        writeln(dat, lname[i]);
+        writeln(dat, phone[i]);     
+    until i = pointer;
+    close(dat);
+    writeln('Written ', i, ' datasets');
+end;
+
+procedure addPhone(first:string; last:string; fone:string);
+begin
     pointer := pointer + 1;
     fname[pointer] := first;
     lname[pointer] := last;
     phone[pointer] := fone;
+    writeFile();
 end;
 
 
@@ -51,8 +66,67 @@ begin
         readln(last);
         write('New phone number: ');
         readln(fone);
-        append(first, last, fone);
+        addPhone(first, last, fone)
     end else writeln('Duplicated First name at ', index);
+end;
+
+procedure amendData();
+var i : integer;
+    f:string;
+begin
+    write('Enter first name to amend ');
+    readln(f);
+    i := getIndex(f);
+    if i <> 0 then begin
+        write('New last name: ');
+        readln(lname[i]);
+        write('New phone number: ');
+        readln(phone[i]);
+        writeFile();
+    end else writeln('Entry not found!');
+end;
+
+procedure searchData();
+var i : integer;
+    f:string;
+begin
+    write('Enter first name to amend ');
+    readln(f);
+    i := getIndex(f);
+    if i <> 0 then begin
+        writeln('Index: ', i);
+        writeln('Name: ', fname[i] , ' ' , lname[i]);
+        writeln('Phone number: ', phone[i]);
+    end else writeln('Entry not found!');
+end;
+
+procedure viewAll();
+var i : integer;
+begin
+    for i := 1 to pointer do begin
+        writeln('Name: ', fname[i] , ' ' , lname[i]);
+        writeln('Phone number: ', phone[i]);
+        writeln('================');
+    end;
+end;
+
+procedure deleteData();
+var i : integer;
+    f:string;
+begin
+    write('Enter first name to delete ');
+    readln(f);
+    i := getIndex(f);
+    if i <> 0 then begin
+        repeat
+            fname[i]:=fname[i+1];
+            lname[i]:=lname[i+1];
+            phone[i]:=phone[i+1];
+            i := i + 1;
+        until i = pointer;
+        pointer := pointer - 1;
+        writeFile();
+    end;
 end;
 
 procedure menu();
@@ -61,16 +135,27 @@ begin
     writeln('Menu');
     writeln('---------');
     writeln('1.Add data');
+    writeln('2.Amend data');
+    writeln('3.Delete data');
+    writeln('4.Search data');
+    writeln('5.View all');
     readln(menuinput);
     case menuinput of
         '1': addData();
-    else writeln('Unknown input!(1-6)');
+        '2': amendData();
+        '3': deleteData();
+        '4': searchData();
+        '5': viewAll();
+    else writeln('Unknown input!(1-5)');
+    end;
 end;
 
 begin
+    assign(dat, 'phone.dat');
     repeat
         readFile();
         menu();
-        writeln('Do you still want to continue? (y/n)');
+        write('Do you still want to continue? (y/n) ');
+        readln(mainloop);
     until mainloop = 'n';
 end.
